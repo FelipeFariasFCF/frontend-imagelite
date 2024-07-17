fc1c9be15799ad6e6f6cbed849175cbc1df3efa9
FelipeFariasFCF <felipe.fariasfcf@outlook.com>
2024-07-17 08:17:58 -0400

    add frontend

---
+++ src/components/input/InputText.tsx
@@ -0,0 +1,17 @@
+interface InputTextProps {
+    style?: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    placeHolder?: string;
+    id?: string;
+    value?: string;
+    type?: string;
+}
+export const InputText: React.FC<InputTextProps> = ({ style, type = "text",  ...outrasProps }: InputTextProps) => {
+    return (
+        <input
+            type={type}
+            {...outrasProps}
+            className={`${style} border px-3 py-2 rounded-lg text-gray-900`}
+        />
+    )
+}
